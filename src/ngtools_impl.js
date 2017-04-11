64affeff75ed06adda242f9621b7064295d20496
Dave Wells <david.wells@mysite.com.au>
2017-04-11 11:36:54 +1000

    Adding fix for router issue

--- src/ngtools_impl.js
+++ src/ngtools_impl.js
@@ -53,7 +53,12 @@ function listLazyRoutesOfModule(entryModule, host, reflector) {
     var allLazyRoutes = lazyRoutes.reduce(function includeLazyRouteAndSubRoutes(allRoutes, lazyRoute) {
         var route = lazyRoute.routeDef.toString();
         _assertRoute(allRoutes, lazyRoute);
-        allRoutes[route] = lazyRoute;
+        		
+		if (route in allRoutes) {
+            return allRoutes;
+        }
+		
+		allRoutes[route] = lazyRoute;
         // StaticReflector does not support discovering annotations like `NgModule` on default
         // exports
         // Which means: if a default export NgModule was lazy-loaded, we can discover it, but,
